0712d0e5efcf8c8bf37cf4fac487897f41b01a5d
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-07-25 17:55:36 +0300

    modified loading screen background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import Image from "next/image";
-import Logo from "../../public/animation2.gif"
+import Logo from "../../public/animation2.gif";
 import { useEffect, useState } from "react";
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -13,14 +13,20 @@ export default function Home() {
 
     return () => clearTimeout(timeout);
   }, []);
-  
+
   return (
     <main className="h-full">
-      {loading && 
-      <div className="bg-[#BFAE94] absolute inset-0 h-screen w-screen">
-      <Image className="h-full w-full object-contain" src={Logo} alt="Animation" height={1080} width={1920}/>
-      </div>
-      }
+      {loading && (
+        <div className="bg-[#bcab91] absolute inset-0 h-screen w-screen">
+          <Image
+            className="h-full w-full object-contain"
+            src={Logo}
+            alt="Animation"
+            height={1080}
+            width={1920}
+          />
+        </div>
+      )}
     </main>
   );
 }
