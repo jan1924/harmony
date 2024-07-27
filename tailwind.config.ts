2085882d329b51453f302d728ffd262303f9c725
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-07-27 23:07:08 +0300

    تعديل المسافة بين الكلمات

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      spacing: {
+        "word-tight": "0.1rem",
+        "word-normal": "0.25rem",
+        "word-wide": "0.5rem",
+      },
     },
   },
   plugins: [],
