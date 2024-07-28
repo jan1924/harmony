86289b92b358fb993323beb9ee674a7341bec924
Mahmoud Ahmed <soufi.m.ahmed@gmail.com>
2024-07-28 16:52:59 +0300

    مسح التعديلات

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
   const [skipped, setSkipped] = useState(false);
 
   return (
-    <main className="h-full flex flex-col bg-[#0e1a2f] word-normal">
+    <main className="h-full flex flex-col gap-8 bg-[#0e1a2f] word-normal">
       <Loader />
 
       {!skipped && (
-        <section className="flex flex-col items-center justify-evenly bg-[#e6e6e6] min-h-svh pt-8 py-16">
+        <section className="flex flex-col items-center justify-evenly bg-[#e6e6e6] min-h-dvh pt-8 py-16">
           <div className="flex items-center gap-4">
             <Image
               className="h-[64px] w-[64px]"
@@ -48,7 +48,7 @@ export default function Home() {
 
       {skipped && (
         <>
-          <section className="flex flex-col items-center justify-between text-right min-h-svh px-8 py-8">
+          <section className="flex flex-col items-center justify-between text-right min-h-dvh px-8 py-8">
             <div className="flex flex-grow flex-col items-center justify-center gap-8 h-full">
               <p
                 className={cn(
@@ -98,139 +98,136 @@ export default function Home() {
             </div>
           </section>
 
-          <section className="min-h-svh bg-[url('../../public/placeholder256.png')] bg-center bg-repeat-round [background-size:128px] md:[background-size:256px]">
-            <div className="container mx-auto flex flex-col gap-6 md:gap-8 items-center justify-center text-right px-8 py-8 ">
-              <div className="flex flex-col gap-2 w-full items-end">
-                <div className="flex gap-4 items-center">
-                  <p
-                    className={cn(
-                      GeSSTwoLight.className,
-                      "text-white text-2xl md:text-4xl"
-                    )}
-                  >
-                    رؤيتنا بعيون أجمل
-                  </p>
-                  <Image
-                    className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
-                    src={"/placeholder96.png"}
-                    alt="Eye icon"
-                    height={96}
-                    width={96}
-                  />
-                </div>
+          <section className="container mx-auto flex flex-col gap-6 md:gap-8 items-center justify-center text-right px-8 py-8 min-h-dvh">
+            <div className="flex flex-col gap-2 w-full items-end">
+              <div className="flex gap-4 items-center">
                 <p
                   className={cn(
                     GeSSTwoLight.className,
-                    "text-white/90 tex md:text-xl md:pr-28"
+                    "text-white text-2xl md:text-4xl"
                   )}
                 >
-                  نعمل باستمرار على تطوير الذائقة العامة في صناعة الحدث التفاعلي
-                  من خلال تقديم فعاليات أكثر ابتكاراً وجمالاً وتناغما مع مضمون
-                  الحدث المقدم للجمهور
+                  رؤيتنا بعيون أجمل
                 </p>
+                <Image
+                  className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+                  src={"/placeholder96.png"}
+                  alt="Eye icon"
+                  height={96}
+                  width={96}
+                />
               </div>
+              <p
+                className={cn(
+                  GeSSTwoLight.className,
+                  "text-white/90 tex md:text-xl md:pr-28"
+                )}
+              >
+                نعمل باستمرار على تطوير الذائقة العامة في صناعة الحدث التفاعلي
+                من خلال تقديم فعاليات أكثر ابتكاراً وجمالاً وتناغما مع مضمون
+                الحدث المقدم للجمهور
+              </p>
+            </div>
 
-              <div className="flex flex-col gap-2 w-full items-end">
-                <div className="flex gap-4 items-center">
-                  <p
-                    className={cn(
-                      GeSSTwoLight.className,
-                      "text-white text-2xl md:text-4xl"
-                    )}
-                  >
-                    رسالتنا بفكر أحدث
-                  </p>
-                  <Image
-                    className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
-                    src={"/placeholder96.png"}
-                    alt="Eye icon"
-                    height={96}
-                    width={96}
-                  />
-                </div>
+            <div className="flex flex-col gap-2 w-full items-end">
+              <div className="flex gap-4 items-center">
                 <p
                   className={cn(
                     GeSSTwoLight.className,
-                    "text-white/90 tex md:text-xl md:pr-28"
+                    "text-white text-2xl md:text-4xl"
                   )}
                 >
-                  طبعا لقصد لغة الإبداع والفنون، باختصار كل حدث تصنع له هوية
-                  خاصة تليق بجمهوره وصانعيه قيمنا أرقى
+                  رسالتنا بفكر أحدث
                 </p>
+                <Image
+                  className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+                  src={"/placeholder96.png"}
+                  alt="Eye icon"
+                  height={96}
+                  width={96}
+                />
               </div>
+              <p
+                className={cn(
+                  GeSSTwoLight.className,
+                  "text-white/90 tex md:text-xl md:pr-28"
+                )}
+              >
+                طبعا لقصد لغة الإبداع والفنون، باختصار كل حدث تصنع له هوية خاصة
+                تليق بجمهوره وصانعيه قيمنا أرقى
+              </p>
+            </div>
 
-              <div className="flex flex-col gap-2 w-full items-end">
-                <div className="flex gap-4 items-center">
-                  <p
-                    className={cn(
-                      GeSSTwoLight.className,
-                      "text-white text-2xl md:text-4xl"
-                    )}
-                  >
-                    قيمنا أرقى
-                  </p>
-                  <Image
-                    className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
-                    src={"/placeholder96.png"}
-                    alt="Eye icon"
-                    height={96}
-                    width={96}
-                  />
-                </div>
+            <div className="flex flex-col gap-2 w-full items-end">
+              <div className="flex gap-4 items-center">
                 <p
                   className={cn(
                     GeSSTwoLight.className,
-                    "text-white/90 tex md:text-xl md:pr-28"
+                    "text-white text-2xl md:text-4xl"
                   )}
                 >
-                  أولاً الابتكار دائما وأبدا البداع شغف الجودة والإتقان في كل
-                  مكان الاحترافية طبيعة
+                  قيمنا أرقى
                 </p>
+                <Image
+                  className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+                  src={"/placeholder96.png"}
+                  alt="Eye icon"
+                  height={96}
+                  width={96}
+                />
               </div>
+              <p
+                className={cn(
+                  GeSSTwoLight.className,
+                  "text-white/90 tex md:text-xl md:pr-28"
+                )}
+              >
+                أولاً الابتكار دائما وأبدا البداع شغف الجودة والإتقان في كل مكان
+                الاحترافية طبيعة
+              </p>
+            </div>
 
-              <div className="flex flex-col gap-2 w-full items-end">
-                <div className="flex gap-4 items-center">
-                  <p
-                    className={cn(
-                      GeSSTwoLight.className,
-                      "text-white text-2xl md:text-4xl"
-                    )}
-                  >
-                    أهدافنا أكبر
-                  </p>
-                  <Image
-                    className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
-                    src={"/placeholder96.png"}
-                    alt="Eye icon"
-                    height={96}
-                    width={96}
-                  />
-                </div>
+            <div className="flex flex-col gap-2 w-full items-end">
+              <div className="flex gap-4 items-center">
                 <p
                   className={cn(
                     GeSSTwoLight.className,
-                    "text-white/90 tex md:text-xl md:pr-28"
+                    "text-white text-2xl md:text-4xl"
                   )}
                 >
-                  العمل على إضافة فكرة إبداعية ومبتكرة تقدم الحدث بشكل فريد و
-                  مميز
-                  <br />
-                  العمل على صناعة لحظات أكثر جاذبية تترك أثرا في أذهان الحضور
-                  مدى الحياة
-                  <br />
-                  العمل على إخراج الحدث بجودة عالية تليق بجمهوره وصانعيه
-                  <br />
-                  العمل على إخراج الحدث بصورة مشرفة تحاكي الفعاليات العالمية
-                  <br />
-                  العمل على دعم قطاع صناعة الفعاليات والمؤتمرات والمعارض بما
-                  يتوافق مع رؤية ٢٠٣٠ العمل على إيجاد فرص عمل تليق بشباب وشابات
-                  الوطن في هذا القطاع
+                  أهدافنا أكبر
                 </p>
+                <Image
+                  className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+                  src={"/placeholder96.png"}
+                  alt="Eye icon"
+                  height={96}
+                  width={96}
+                />
               </div>
+              <p
+                className={cn(
+                  GeSSTwoLight.className,
+                  "text-white/90 tex md:text-xl md:pr-28"
+                )}
+              >
+                العمل على إضافة فكرة إبداعية ومبتكرة تقدم الحدث بشكل فريد و مميز
+                <br />
+                العمل على صناعة لحظات أكثر جاذبية تترك أثرا في أذهان الحضور مدى
+                الحياة
+                <br />
+                العمل على إخراج الحدث بجودة عالية تليق بجمهوره وصانعيه
+                <br />
+                العمل على إخراج الحدث بصورة مشرفة تحاكي الفعاليات العالمية
+                <br />
+                العمل على دعم قطاع صناعة الفعاليات والمؤتمرات والمعارض بما
+                يتوافق مع رؤية ٢٠٣٠ العمل على إيجاد فرص عمل تليق بشباب وشابات
+                الوطن في هذا القطاع
+              </p>
             </div>
           </section>
 
-          <section className="container mx-auto relative flex flex-col items-center justify-center text-right min-h-svh px-8 py-8">
+          <section className="container mx-auto relative flex flex-col items-center justify-center text-right min-h-dvh px-8 py-8">
             <div className="absolute top-8 right-4 md:right-8 flex items-center gap-2 justify-end w-full">
               <Image
                 className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
