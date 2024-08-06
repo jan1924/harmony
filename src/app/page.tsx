dbe32e585b74178510ae0a9620f6ceec8be2beaf
jan1924 <abdulrahman.jilan@gmail.com>
2024-08-06 23:08:09 +0300

    deleted pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,59 @@ import { useState } from "react";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import Loader from "@/components/Loader";
-import { Download } from "lucide-react";
+import { Download, MoveLeft } from "lucide-react";
+import Link from "next/link";
+import Card from "@/components/Card";
 const TajawalRegular = localFont({
   src: "../../public/fonts/Tajawal_Regular.ttf",
 });
 export default function Home() {
   const [skipped, setSkipped] = useState(false);
+  const SERVICES = [
+    {
+      id: 1,
+      name: "اختبار المساحات",
+      icon: "/searchicon.png",
+      image: "/searchImage.jpg",
+    },
+    {
+      id: 2,
+      name: "الضيافة",
+      icon: "/hospitality.png",
+      image: "/hospitalityImage.jpg",
+    },
+    {
+      id: 3,
+      name: "التخطيط والإبتكار للحدث",
+      icon: "/planning.png",
+      image: "/planningImage.jpg",
+    },
+    {
+      id: 4,
+      name: "تصميم وتنسيق الديكور والبوثات والمسارح ومرافق احدث",
+      icon: "/design.png",
+      image: "/designImage.jpg",
+    },
+    {
+      id: 5,
+      name: "تجهيز المساحة بالأنظمة الصوتية",
+      icon: "/mic.png",
+      image: "/micImage.jpg",
+    },
+    {
+      id: 6,
+      name: "المطبوعات والهدايا وبطاقات الدعوة",
+      icon: "/gift.png",
+      image: "/giftImage.jpg",
+    },
+    {
+      id: 7,
+      name: "بناء الثيم والهوية البصرية للحدث",
+      icon: "/leaf.png",
+      image: "/leafImage.jpg",
+    },
+  ];
+
 
   return (
     <main className="h-full flex flex-col gap-8 bg-[#0e1a2f] word-normal">
@@ -120,6 +167,259 @@ export default function Home() {
             </div>
           </section>
 
+          <section className="container mx-auto flex flex-col gap-6 md:gap-8 items-center justify-center text-right px-8 py-8 min-h-svh">
+        <div className="flex flex-col gap-2 w-full items-end">
+          <div className="flex gap-4 items-center">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-[#a08e6c] font-semibold"
+              )}
+            >
+              رؤيتنا بعيون أجمل
+            </p>
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/eyesIcon.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />
+          </div>
+          <p
+            className={cn(
+              TajawalRegular.className,
+              "text-white/90 tex md:text-xl md:pr-28"
+            )}
+          >
+            نعمل باستمرار على تطوير الذائقة العامة في صناعة الحدث التفاعلي من
+            خلال تقديم فعاليات أكثر ابتكاراً وجمالاً وتناغما مع مضمون الحدث
+            المقدم للجمهور
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2 w-full items-end">
+          <div className="flex gap-4 items-center">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-[#a08e6c] font-semibold"
+              )}
+            >
+              رسالتنا بفكر أحدث
+            </p>
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/stick.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />
+          </div>
+          <p
+            className={cn(
+              TajawalRegular.className,
+              "text-white/90 tex md:text-xl md:pr-28"
+            )}
+          >
+            طبعا لقصد لغة الإبداع والفنون، باختصار كل حدث تصنع له هوية خاصة تليق
+            بجمهوره وصانعيه قيمنا أرقى
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2 w-full items-end">
+          <div className="flex gap-4 items-center">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-[#a08e6c] font-semibold"
+              )}
+            >
+              قيمنا أرقى
+            </p>
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/handIcon.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />
+          </div>
+          <p
+            className={cn(
+              TajawalRegular.className,
+              "text-white/90 tex md:text-xl md:pr-28"
+            )}
+          >
+            الجمال
+            <br />
+            الإبتكار
+            <br />
+            الابداع
+            <br />
+            الجودة
+            <br />
+            الاحترافية
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-2 w-full items-end">
+          <div className="flex gap-4 items-center">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-[#a08e6c] font-semibold"
+              )}
+            >
+              أهدافنا أكبر
+            </p>
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/corr.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />
+          </div>
+          <p
+            className={cn(
+              TajawalRegular.className,
+              "text-white/90 tex md:text-xl md:pr-28"
+            )}
+          >
+            العمل على إضافة فكرة إبداعية ومبتكرة تقدم الحدث بشكل فريد و مميز
+            <br />
+            العمل على صناعة لحظات أكثر جاذبية تترك أثرا في أذهان الحضور مدى
+            الحياة
+            <br />
+            العمل على إخراج الحدث بجودة عالية تليق بجمهوره وصانعيه
+            <br />
+            العمل على إخراج الحدث بصورة مشرفة تحاكي الفعاليات العالمية
+            <br />
+            العمل على دعم قطاع صناعة الفعاليات والمؤتمرات والمعارض بما يتوافق مع
+            رؤية ٢٠٣٠ العمل على إيجاد فرص عمل تليق بشباب وشابات الوطن في هذا
+            القطاع
+          </p>
+        </div>
+        <Link href={"/services"}>
+          <div className="flex items-center gap-4">
+            <p className="text-white text-lg">دعنا ننظم حدثك</p>
+            <MoveLeft color="white" size={48} />
+          </div>
+        </Link>
+      </section>
+      {/* <section className="bg-[#f3f2f0] bg-[url('../../public/bglogo.png')] bg-center bg-repeat-round [background-size:128px] md:[background-size:256px]">
+        <div className="container mx-auto relative flex flex-col gap-8 items-center justify-center text-right min-h-[300px] px-8 py-8">
+          <div className="flex justify-center items-center">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-center"
+              )}
+            >
+              تواصل معنا لنتحدث عن حدثك التالي
+            </p>
+          </div>
+          <div className="flex justify-evenly md:justify-center md:gap-32 items-center w-full">
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/mail.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />{" "}
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/whatsapp.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />
+          </div>
+        </div>
+      </section> */}
+
+     
+  
+      <section className="flex flex-col items-center bg-bottom [background-size:128px] md:[background-size:256px] [background-repeat:repeat-x]">
+        <div className="container mx-auto relative flex flex-col items-center justify-center text-right px-8 py-8 min-h-svh">
+          <div className="flex items-center gap-2 justify-end w-full pb-8">
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/logo.jpg"}
+              alt=""
+              height={96}
+              width={96}
+            />
+            <p className="text-3xl text-white">HARMONY</p>
+          </div>
+          <div className="flex flex-col items-end gap-8 w-full text-white md:pb-20">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-[#a08e6c] font-semibold"
+              )}
+            >
+              خدماتنا الأفضل
+            </p>
+            <div className="flex flex-col gap-8 w-full">
+              <div
+                className={cn(
+                  TajawalRegular.className,
+                  "grid grid-cols-2 md:grid-cols-3 items-center justify-center gap-4 md:gap-8"
+                )}
+              >
+                {SERVICES.map((service) => (
+                  <Card
+                    id={service.id}
+                    key={service.id}
+                    name={service.name}
+                    icon={service.icon}
+                    image={service.image}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <Link href={"/contact"}>
+          <div className="flex items-center gap-4">
+            <p className="text-white text-lg">اطلب خدمتك الان</p>
+            <MoveLeft color="white" size={48} />
+          </div>
+        </Link>
+      </section>
+      {/* <section className="bg-[#f3f2f0] bg-[url('../../public/bglogo.png')] bg-center bg-repeat-round [background-size:128px] md:[background-size:256px]">
+        <div className="container mx-auto relative flex flex-col gap-8 items-center justify-center text-right min-h-[300px] px-8 py-8">
+          <div className="flex justify-center items-center">
+            <p
+              className={cn(
+                TajawalRegular.className,
+                "text-2xl md:text-4xl text-center"
+              )}
+            >
+              تواصل معنا لنتحدث عن حدثك التالي
+            </p>
+          </div>
+          <div className="flex justify-evenly md:justify-center md:gap-32 items-center w-full">
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/mail.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />{" "}
+            <Image
+              className="h-[48px] w-[48px] md:h-[96px] md:w-[96px]"
+              src={"/whatsapp.png"}
+              alt="Eye icon"
+              height={96}
+              width={96}
+            />
+          </div>
+        </div>
+      </section> */}
+
           <section className="container min-h-[200svh] flex flex-col items-center justify-evenly">
             <div className="flex flex-col gap-4 items-center">
               <p
@@ -172,7 +472,7 @@ export default function Home() {
             </div>
           </section>
 
-          <section className="bg-[#f3f2f0] bg-[url('../../public/bglogo.png')] bg-center bg-repeat-round [background-size:128px] md:[background-size:256px]">
+          {/* <section className="bg-[#f3f2f0] bg-[url('../../public/bglogo.png')] bg-center bg-repeat-round [background-size:128px] md:[background-size:256px]">
             <div className="container mx-auto relative flex flex-col gap-8 items-center justify-center text-right min-h-[300px] px-8 py-8">
               <div className="flex justify-center items-center">
                 <p
@@ -201,7 +501,7 @@ export default function Home() {
                 />
               </div>
             </div>
-          </section>
+          </section> */}
         </>
       )}
     </main>
